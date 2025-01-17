e83f5887dabf22cb381830f9587763af052e720e
Joao Heslin <joaoheslin9@gmail.com>
2025-01-17 18:30:54 -0300

    Completed

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ elif option == "Main Causes":
 
     Here are some key insights from our analysis:
     """)
+    st.divider()
 
     #Age and Depression
     col1, = st.columns(1)
@@ -84,6 +85,7 @@ elif option == "Main Causes":
     st.write("""
     - **Age**: We observed a trend indicating that younger individuals are more likely to experience depression.  
     """)
+    st.divider()
 
     #Study Satisfaction
     col2, = st.columns(1)
@@ -100,6 +102,7 @@ elif option == "Main Causes":
     st.write("""
              - Study Satisfaction: **There is a correlation between satisfaction with studies and depression, suggesting that the lower the satisfaction, the greater the likelihood of developing depression.**
                 """)
+    st.divider()
     
     #Academic Pressure and Depression
     col3, = st.columns(1)
@@ -115,6 +118,7 @@ elif option == "Main Causes":
     st.write("""
              - Academic Pressure: **We noticed a trend that the more academic pressure a person faces, the higher the chance of experiencing depression.**
                 """)
+    st.divider()
              
     #Dietary Habits
     col4, = st.columns(1)
@@ -132,6 +136,7 @@ elif option == "Main Causes":
     st.write("""
              - Dietary Habits: **We noticed that individuals with eating habits classified as "unhealthy" are much more likely to develop depression than those with eating habits considered healthier.**
              """)
+    st.divider()
     
     #Financial Stress
     col5, = st.columns(1)
@@ -147,6 +152,7 @@ elif option == "Main Causes":
     st.write("""
              - Financial Stress: **People who face high financial stress are much more likely to develop depression, while those with low financial stress are not prone to illness.**
              """)
+    st.divider()
     
     # Suicidal Thoughts
     col6, = st.columns(1)
@@ -171,4 +177,109 @@ elif option == "Main Causes":
             text-align: center;
         }
     </style>
-    """, unsafe_allow_html=True)    
+    """, unsafe_allow_html=True)
+    
+else:
+    st.write("""# Depression Prediction""")
+    st.write("""
+        ### Machine Learning Model
+        > We developed a machine learning model to predict the likelihood of depression based on individual data. The model uses a dataset of almost 28,000 individuals and their responses to various questions related to mental health, lifestyle, and personal experiences.
+        - Answer the following questions to get the prediction.
+        """)
+    st.divider()
+    
+    #Feature Engineering
+    #Label Encoding
+    le = LabelEncoder()
+    label_encoder = df.select_dtypes(include='object').columns
+    for i in label_encoder:
+        df[i] = le.fit_transform(df[i])
+    # Smote
+    smote = SMOTE()
+    x = df.drop(columns=["Depression"],axis=1)
+    y = df["Depression"]
+    X_resampled, y_resampled = smote.fit_resample(x,y)
+    df_resampled = pd.concat([X_resampled, y_resampled], axis=1)
+
+    # Splitting the data
+    x = df_resampled.drop(columns=["Depression", "Family History of Mental Illness", "Sleep Duration", "Gender",
+                                "City", "Degree"],axis=1)
+    y = df_resampled["Depression"]
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=42)
+
+    #Standard Scaling
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(x_train)
+    X_test = scaler.transform(x_test)
+
+    #Model
+    catboost_model = CatBoostClassifier(task_type='CPU', eval_metric='Accuracy', iterations= 200, learning_rate =  0.1, 
+                                        max_depth= 3, random_state= 1, verbose=0).fit(X_train, y_train)
+        
+    col1, col2 = st.columns(2)
+    col3, col4 = st.columns(2)
+    col5, col6 = st.columns(2)
+    col7, col8 = st.columns(2)
+    
+    with col1:
+        age = st.slider("Enter your Age", min_value=0, max_value=100, value=0, step=1)
+        
+    with col2:
+        dietary = st.selectbox("Select your Dietary Habits", 
+                            ["Healthy", "Moderate", "Unhealthy", "Others"])
+            
+    with col3:
+        study = st.selectbox("Select your Study Satisfaction", range(0,6))
+    
+    with col4:
+        cgpa = st.slider("Enter your CGPA (Cumulative Grade Point Average)", 
+                        min_value=1.0, max_value=10.0, value=1.0, step=0.1)
+    
+    with col5:
+        work_study = st.slider("How many hours do you work or study per day?", min_value=0.0, 
+        max_value=24.0, value=8.0, step=0.5)
+        
+    with col6:
+        stress = st.selectbox("Report your level of financial stress", range(1,6))
+        
+    with col7:
+        academic = st.selectbox("Rate your academic pressure", range(0,6))
+    
+    with col8:
+        thoughts = st.selectbox("Have you ever had suicidal thoughts?", ["No", "Yes"])
+
+    st.markdown("""
+    <style>
+        h1, h3 {
+            text-align: center;
+        }
+    </style>
+        """, unsafe_allow_html=True)
+    st.divider()
+    
+    patient = np.array([
+        [age,
+         0 if dietary == "Healthy" else (1 if dietary == "Moderate" else (3 if dietary == "Unhealthy" else 4)),
+         study,
+         cgpa,
+         work_study,
+         stress,
+         academic,
+         1 if thoughts == "Yes" else 0]
+    ])
+    
+    if st.button('Predict Depression Tendency'):
+
+        patient_scaled = scaler.transform(patient)
+        result = catboost_model.predict(patient_scaled)
+        
+        if result == 1:
+            st.markdown(
+                "<h3 style='color: red;'>The individual HAS tendencies towards Depression</h3>", 
+                unsafe_allow_html=True
+        )
+        else:
+            st.markdown(
+                "<h3 style='color: green;'>The individual DOES NOT HAVE tendencies towards Depression</h3>", 
+                unsafe_allow_html=True
+        )
